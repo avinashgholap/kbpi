a4c1a9b89bc7311658b587ede7ef3484481f8835
avinashgholap <avinashgholap123@gmail.com>
2025-09-26 23:04:24 +0530

    Navbar and Home component added

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from '../home/home.component';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    HomeComponent,
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {
+  @Input() title: string = '';
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  log() {}
+  home() {}
+  logout() {}
+  toggleSidenav() {
+    if (this.sidenav) {
+      this.sidenav.toggle();
+    }
+  }
+  closeSidenav() {
+    if (this.sidenav) {
+      this.sidenav.close();
+    }
+  }
+}
