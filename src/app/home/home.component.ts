04a9a2c92338d0c77ae0f5a5a07388252152feb0
avinashgholap <avinashgholap123@gmail.com>
2025-09-28 09:30:12 +0530

    Contact us today opens same contact modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { ContactModalComponent } from '../contact/contact-modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  contactForm: FormGroup;
+
+  constructor(private dialog: MatDialog, private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      phone: [
+        '',
+        [Validators.required, Validators.pattern(/^[\+]?[1-9][\d]{0,15}$/)],
+      ],
+      companyName: ['', [Validators.required, Validators.minLength(2)]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  openContactModal(): void {
+    const dialogRef = this.dialog.open(ContactModalComponent, {
+      width: '90vw',
+      maxWidth: '600px',
+      maxHeight: '90vh',
+      data: { form: this.contactForm },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        // Form was submitted successfully
+        console.log('Form submitted:', this.contactForm.value);
+        // Reset form after successful submission
+        this.contactForm.reset();
+      }
+    });
+  }
+}
